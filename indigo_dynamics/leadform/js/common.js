842b1e1c3f4e85d6d41dff73b69e83ddb5ba7c4c
Misha Lagutin <awesomewebmail@gmail.com>
2016-06-20 17:36:04 +0300

    indigo_dynamics: comment empty states jsons

--- indigo_dynamics/leadform/js/common.js
+++ indigo_dynamics/leadform/js/common.js
@@ -4,76 +4,68 @@ $(document).ready(function () {
 
   var WAIT_FOR_REDIRECT = 60000;
 
-  var _states = [{"name": "State", "abbreviation": ""}, {"name": "Alabama", "abbreviation": "AL"}, {
-    "name": "Alaska",
-    "abbreviation": "AK"
-  }, {"name": "American Samoa", "abbreviation": "AS"}, {"name": "Arizona", "abbreviation": "AZ"}, {
-    "name": "Arkansas",
-    "abbreviation": "AR"
-  }, {"name": "California", "abbreviation": "CA"}, {"name": "Colorado", "abbreviation": "CO"}, {
-    "name": "Connecticut",
-    "abbreviation": "CT"
-  }, {"name": "Delaware", "abbreviation": "DE"}, {"name": "Dist of Columbia", "abbreviation": "DC"}, {
-    "name": "Florida",
-    "abbreviation": "FL"
-  }, {"name": "Georgia", "abbreviation": "GA"}, {"name": "Guam", "abbreviation": "GU"}, {
-    "name": "Hawaii",
-    "abbreviation": "HI"
-  }, {"name": "Idaho", "abbreviation": "ID"}, {"name": "Illinois", "abbreviation": "IL"}, {
-    "name": "Indiana",
-    "abbreviation": "IN"
-  }, {"name": "Iowa", "abbreviation": "IA"}, {"name": "Kansas", "abbreviation": "KS"}, {
-    "name": "Kentucky",
-    "abbreviation": "KY"
-  }, {"name": "Louisiana", "abbreviation": "LA"}, {"name": "Maine", "abbreviation": "ME"}, {
-    "name": "Maryland",
-    "abbreviation": "MD"
-  }, {"name": "Marshall Islands", "abbreviation": "MH"}, {
-    "name": "Massachusetts",
-    "abbreviation": "MA"
-  }, {"name": "Michigan", "abbreviation": "MI"}, {"name": "Micronesia", "abbreviation": "FM"}, {
-    "name": "Minnesota",
-    "abbreviation": "MN"
-  }, {"name": "Mississippi", "abbreviation": "MS"}, {
-    "name": "Missouri",
-    "abbreviation": "MO"
-  }, {"name": "Montana\tMont", "abbreviation": "MT"}, {"name": "Nebraska", "abbreviation": "NE"}, {
-    "name": "Nevada",
-    "abbreviation": "NV"
-  }, {"name": "New Hampshire", "abbreviation": "NH"}, {
-    "name": "New Jersey",
-    "abbreviation": "NJ"
-  }, {"name": "New Mexico", "abbreviation": "NM"}, {
-    "name": "New York",
-    "abbreviation": "NY"
-  }, {"name": "North Carolina", "abbreviation": "NC"}, {
-    "name": "North Dakota",
-    "abbreviation": "ND"
-  }, {"name": "Northern Marianas", "abbreviation": "MP"}, {"name": "Ohio", "abbreviation": "OH"}, {
-    "name": "Oklahoma",
-    "abbreviation": "OK"
-  }, {"name": "Oregon", "abbreviation": "OR"}, {"name": "Palau", "abbreviation": "PW"}, {
-    "name": "Pennsylvania",
-    "abbreviation": "PA"
-  }, {"name": "Puerto Rico", "abbreviation": "PR"}, {
-    "name": "Rhode Island",
-    "abbreviation": "RI"
-  }, {"name": "South Carolina", "abbreviation": "SC"}, {
-    "name": "South Dakota",
-    "abbreviation": "SD"
-  }, {"name": "Tennessee", "abbreviation": "TN"}, {"name": "Texas", "abbreviation": "TX"}, {
-    "name": "Utah",
-    "abbreviation": "UT"
-  }, {"name": "Vermont\tVt", "abbreviation": "VT"}, {
-    "name": "Virginia",
-    "abbreviation": "VA"
-  }, {"name": "Virgin Islands", "abbreviation": "VI"}, {
-    "name": "Washington",
-    "abbreviation": "WA"
-  }, {"name": "West Virginia", "abbreviation": "WV"}, {
-    "name": "Wisconsin",
-    "abbreviation": "WI"
-  }, {"name": "Wyoming\tWyo", "abbreviation": "WY"}];
+  var _states = [
+    {"name": "State", "abbreviation": ""},
+    {"name": "Alabama", "abbreviation": "AL"},
+    {"name": "Alaska", "abbreviation": "AK"},
+    //{"name": "American Samoa", "abbreviation": "AS"},
+    {"name": "Arizona", "abbreviation": "AZ"},
+    {"name": "Arkansas", "abbreviation": "AR"},
+    {"name": "California", "abbreviation": "CA"},
+    {"name": "Colorado", "abbreviation": "CO"},
+    {"name": "Connecticut", "abbreviation": "CT"},
+    {"name": "Delaware", "abbreviation": "DE"},
+    {"name": "Dist of Columbia", "abbreviation": "DC"},
+    {"name": "Florida", "abbreviation": "FL"},
+    {"name": "Georgia", "abbreviation": "GA"},
+    //{"name": "Guam", "abbreviation": "GU"},
+    {"name": "Hawaii", "abbreviation": "HI"},
+    {"name": "Idaho", "abbreviation": "ID"},
+    {"name": "Illinois", "abbreviation": "IL"},
+    {"name": "Indiana", "abbreviation": "IN"},
+    {"name": "Iowa", "abbreviation": "IA"},
+    {"name": "Kansas", "abbreviation": "KS"},
+    {"name": "Kentucky", "abbreviation": "KY"},
+    {"name": "Louisiana", "abbreviation": "LA"},
+    {"name": "Maine", "abbreviation": "ME"},
+    {"name": "Maryland", "abbreviation": "MD"},
+    //{"name": "Marshall Islands", "abbreviation": "MH"},
+    {"name": "Massachusetts", "abbreviation": "MA"},
+    {"name": "Michigan", "abbreviation": "MI"},
+    //{"name": "Micronesia", "abbreviation": "FM"},
+    {"name": "Minnesota", "abbreviation": "MN"},
+    {"name": "Mississippi", "abbreviation": "MS"},
+    {"name": "Missouri", "abbreviation": "MO"},
+    {"name": "Montana\tMont", "abbreviation": "MT"},
+    {"name": "Nebraska", "abbreviation": "NE"},
+    {"name": "Nevada", "abbreviation": "NV"},
+    {"name": "New Hampshire", "abbreviation": "NH"},
+    {"name": "New Jersey", "abbreviation": "NJ"},
+    {"name": "New Mexico", "abbreviation": "NM"},
+    {"name": "New York", "abbreviation": "NY"},
+    {"name": "North Carolina", "abbreviation": "NC"},
+    {"name": "North Dakota", "abbreviation": "ND"},
+    //{"name": "Northern Marianas", "abbreviation": "MP"},
+    {"name": "Ohio", "abbreviation": "OH"},
+    {"name": "Oklahoma", "abbreviation": "OK"},
+    {"name": "Oregon", "abbreviation": "OR"},
+    //{"name": "Palau", "abbreviation": "PW"},
+    {"name": "Pennsylvania", "abbreviation": "PA"},
+    //{"name": "Puerto Rico", "abbreviation": "PR"},
+    {"name": "Rhode Island", "abbreviation": "RI"},
+    {"name": "South Carolina", "abbreviation": "SC"},
+    {"name": "South Dakota", "abbreviation": "SD"},
+    {"name": "Tennessee", "abbreviation": "TN"},
+    {"name": "Texas", "abbreviation": "TX"},
+    {"name": "Utah", "abbreviation": "UT"},
+    {"name": "Vermont\tVt", "abbreviation": "VT"},
+    {"name": "Virginia", "abbreviation": "VA"},
+    //{"name": "Virgin Islands", "abbreviation": "VI"},
+    {"name": "Washington", "abbreviation": "WA"},
+    {"name": "West Virginia", "abbreviation": "WV"},
+    {"name": "Wisconsin", "abbreviation": "WI"},
+    {"name": "Wyoming\tWyo", "abbreviation": "WY"}
+  ];
 
   var _cities = {};
 
